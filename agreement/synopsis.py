568bf8835145e6be43f8e638a34a2f160827f6e1
rowdenw <rowdenw@me.com>
2024-07-08 12:34:44 -0700

    Mark "TODO", rename, and reorganize.

--- agreement/synopsis.py
+++ agreement/synopsis.py
@@ -5,58 +5,10 @@ import difflib
 import pandas as pd
 
 
-def highlight_matching_lemmata(left_text, right_text):
-    cltk_nlp = NLP(language="grc", suppress_banner=True)
-
-    left_doc = cltk_nlp.analyze(text=left_text)
-    left_lemmata = left_doc.lemmata
-    left_POS = left_doc.pos
-    left_tokens = left_doc.tokens
-
-    right_doc = cltk_nlp.analyze(text=right_text)
-    right_lemmata = right_doc.lemmata
-    right_POS = right_doc.pos
-    right_tokens = right_doc.tokens
-
-    left_result = ""
-    right_result = ""
-    prev = difflib.Match(0, 0, 0)
-    matcher = difflib.SequenceMatcher(a=left_lemmata, b=right_lemmata)
-    for match in matcher.get_matching_blocks():
-        if prev.a + prev.size != match.a:
-            for i in range(prev.a + prev.size, match.a):
-                if left_POS[i] == "PUNCT" or match.a == 0:
-                    left_result += left_tokens[i]
-                else:
-                    left_result += " " + left_tokens[i]
-        if prev.b + prev.size != match.b:
-            for i in range(prev.b + prev.size, match.b):
-                if right_POS[i] == "PUNCT" or match.b == 0:
-                    right_result += right_tokens[i]
-                else:
-                    right_result += " " + right_tokens[i]
-        if match.a < len(left_lemmata) or match.b < len(right_lemmata) or match.size > 0:
-            left_result += "[yellow]"
-            for i in range(match.a, match.a + match.size):
-                if left_POS[i] == "PUNCT":
-                    left_result += left_tokens[i]
-                else:
-                    left_result += " " + left_tokens[i]
-            left_result += "[/yellow]"
-            right_result += "[yellow]"
-            for i in range(match.b, match.b + match.size):
-                if right_POS[i] == "PUNCT":
-                    right_result += right_tokens[i]
-                else:
-                    right_result += " " + right_tokens[i]
-            right_result += "[/yellow]"
-            prev = match
-    return left_result, right_result
-
-
 class Synopsis:
     def __init__(self, title, **kwargs):
-        self.df = pd.DataFrame()
+        self.data = pd.DataFrame()
+        #TODO:self.summary = pd.DataFrame()
         self.table = Table()
         self.table.title = title
         if kwargs.get("left_passage"):
@@ -68,17 +20,74 @@ class Synopsis:
             left_text = kwargs["left_text"]
             right_text = kwargs["right_text"]
             # TODO: This could produce an error if there is text but no passage. Write test to raise error.
-            left_data, right_data = highlight_matching_lemmata(left_text, right_text)
-            self.df[kwargs["left_passage"]] = [left_data]
-            self.df[kwargs["right_passage"]] = [right_data]
+            left_data, right_data = self.match_lemmata(
+                left_text, right_text
+            )
+            self.data[kwargs["left_passage"]] = [left_data]
+            self.data[kwargs["right_passage"]] = [right_data]
             self.table.add_row(left_data, right_data)
         elif kwargs.get("left_text"):
             self.table.add_row(kwargs["left_text"])
         elif kwargs.get("right_text"):
             self.table.add_row(kwargs["right_text"])
 
-    def getDataFrame(self):
-        return self.df
+    def getData(self):
+        return self.data
+
+    def match_lemmata(self, left_text, right_text):
+        cltk_nlp = NLP(language="grc", suppress_banner=True)
+
+        left_doc = cltk_nlp.analyze(text=left_text)
+        left_lemmata = left_doc.lemmata
+        left_POS = left_doc.pos
+        left_tokens = left_doc.tokens
+
+        right_doc = cltk_nlp.analyze(text=right_text)
+        right_lemmata = right_doc.lemmata
+        right_POS = right_doc.pos
+        right_tokens = right_doc.tokens
+
+        left_result = ""
+        right_result = ""
+        prev = difflib.Match(0, 0, 0)
+        matcher = difflib.SequenceMatcher(a=left_lemmata, b=right_lemmata)
+        for match in matcher.get_matching_blocks():
+            if prev.a + prev.size != match.a:
+                for i in range(prev.a + prev.size, match.a):
+                    if left_POS[i] == "PUNCT" or match.a == 0:
+                        left_result += left_tokens[i]
+                    else:
+                        left_result += " " + left_tokens[i]
+            if prev.b + prev.size != match.b:
+                for i in range(prev.b + prev.size, match.b):
+                    if right_POS[i] == "PUNCT" or match.b == 0:
+                        right_result += right_tokens[i]
+                    else:
+                        right_result += " " + right_tokens[i]
+            if (
+                match.a < len(left_lemmata)
+                or match.b < len(right_lemmata)
+                or match.size > 0
+            ):
+                left_result += "[yellow]"
+                for i in range(match.a, match.a + match.size):
+                    if left_POS[i] == "PUNCT":
+                        left_result += left_tokens[i]
+                    else:
+                        left_result += " " + left_tokens[i]
+                left_result += "[/yellow]"
+                right_result += "[yellow]"
+                for i in range(match.b, match.b + match.size):
+                    if right_POS[i] == "PUNCT":
+                        right_result += right_tokens[i]
+                    else:
+                        right_result += " " + right_tokens[i]
+                right_result += "[/yellow]"
+                prev = match
+        return left_result, right_result
 
     def makeTable(self):
         return self.table
+
+    #TODO:def getSummary(self):
+        #TODO:return self.summary
--- tests/test_make_table.py
+++ tests/test_make_table.py
@@ -38,13 +38,15 @@ class TestMakeTable(unittest.TestCase):
             left_text=config.grc_byz1904_ΚΑΤΑ_ΜΑΡΚΟΝ_13_21,
             right_text=config.grc_byz1904_ΚΑΤΑ_ΜΑΤΘΑΙΟΝ_24_23,
         )
-        data = synopsis.getDataFrame()
+        data = synopsis.getData()
         left_highlighted = " καὶ[yellow] τότε ἐάν τις ὑμῖν εἴπῃ, ἰδοὺ ὧδε ὁ Χριστός[/yellow], ἰδοὺ ἐκεῖ[yellow], μὴ πιστεύετε[/yellow]."
         self.assertEqual(data.iat[0, 0], left_highlighted)
         self.assertEqual(data.iloc[0]["Mark 13:21"], left_highlighted)
         right_highlighted = "[yellow] τότε ἐάν τις ὑμῖν εἴπῃ, ἰδοὺ ὧδε ὁ Χριστὸς[/yellow] ἢ ὧδε[yellow], μὴ πιστεύσητε[/yellow]·"
         self.assertEqual(data.iat[0, 1], right_highlighted)
         self.assertEqual(data.iloc[0]["Matt. 24:23"], right_highlighted)
+        #TODO: summary = synopsis.getSummary()
+        #TODO: self.assertEqual(summary.iloc["count"]["Mark 13:21"], 14)
 
 
 if __name__ == "__main__":
