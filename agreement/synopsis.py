7fbcb17560548d56cc9365b3ef9ba64a3fb58493
rowdenw <rowdenw@me.com>
2024-07-08 13:46:18 -0700

    Document methods and transpose data.

--- agreement/synopsis.py
+++ agreement/synopsis.py
@@ -8,7 +8,7 @@ import pandas as pd
 class Synopsis:
     def __init__(self, title, **kwargs):
         self.data = pd.DataFrame()
-        #TODO:self.summary = pd.DataFrame()
+
         self.table = Table()
         self.table.title = title
         if kwargs.get("left_passage"):
@@ -17,14 +17,16 @@ class Synopsis:
             self.table.add_column(kwargs["right_passage"])
         # TODO: Write tests to produce empty string if passage provided but no text.
         if kwargs.get("left_text") and kwargs.get("right_text"):
+            self.data["passage"] = [
+                kwargs.get("left_passage"),
+                kwargs.get("right_passage"),
+            ]
             left_text = kwargs["left_text"]
             right_text = kwargs["right_text"]
+            self.data["raw text"] = [kwargs["left_text"], kwargs["right_text"]]
             # TODO: This could produce an error if there is text but no passage. Write test to raise error.
-            left_data, right_data = self.match_lemmata(
-                left_text, right_text
-            )
-            self.data[kwargs["left_passage"]] = [left_data]
-            self.data[kwargs["right_passage"]] = [right_data]
+            left_data, right_data = self.match_lemmata(left_text, right_text)
+            self.data["highlighted text"] = [left_data, right_data]
             self.table.add_row(left_data, right_data)
         elif kwargs.get("left_text"):
             self.table.add_row(kwargs["left_text"])
@@ -32,6 +34,18 @@ class Synopsis:
             self.table.add_row(kwargs["right_text"])
 
     def getData(self):
+        """
+        Get data resulting from analysis, e.g., for testing.
+
+        Returns
+        -------
+        pandas.DataFrame
+            tabular data with one column per analysis result and one row per passage
+
+        See Also
+        --------
+        Synopsis.getTable : transposed data in printable form
+        """
         return self.data
 
     def match_lemmata(self, left_text, right_text):
@@ -86,8 +100,17 @@ class Synopsis:
                 prev = match
         return left_result, right_result
 
-    def makeTable(self):
-        return self.table
+    def getTable(self):
+        """
+        Get table showing text analysis.
 
-    #TODO:def getSummary(self):
-        #TODO:return self.summary
+        Returns
+        -------
+        rich.table
+            table with one column per passage and rows of text and analysis, suitable for printing to console or SVG
+
+        See Also
+        --------
+        Synopsis.getData : individual analysis results that are transposed for the table
+        """
+        return self.table
--- tests/test_make_table.py
+++ tests/test_make_table.py
@@ -11,7 +11,7 @@ class TestMakeTable(unittest.TestCase):
             left_passage="Mark 13:21",
             left_text=config.grc_byz1904_ΚΑΤΑ_ΜΑΡΚΟΝ_13_21,
         )
-        table = synopsis.makeTable()
+        table = synopsis.getTable()
         self.assertEqual(table.title, "291 False Christs and False Prophets")
         self.assertEqual(table.columns[0].header, "Mark 13:21")
         self.assertEqual(table.row_count, 1)
@@ -24,7 +24,7 @@ class TestMakeTable(unittest.TestCase):
             left_text=config.grc_byz1904_ΚΑΤΑ_ΜΑΡΚΟΝ_13_21,
             right_text=config.grc_byz1904_ΚΑΤΑ_ΜΑΤΘΑΙΟΝ_24_23,
         )
-        table = synopsis.makeTable()
+        table = synopsis.getTable()
         self.assertEqual(table.title, "291 False Christs and False Prophets")
         self.assertEqual(table.columns[0].header, "Mark 13:21")
         self.assertEqual(table.columns[1].header, "Matt. 24:23")
@@ -39,14 +39,17 @@ class TestMakeTable(unittest.TestCase):
             right_text=config.grc_byz1904_ΚΑΤΑ_ΜΑΤΘΑΙΟΝ_24_23,
         )
         data = synopsis.getData()
+        self.assertEqual(data.loc[0, "passage"], "Mark 13:21")
+        self.assertEqual(data.loc[1, "passage"], "Matt. 24:23")
+        self.assertEqual(data.loc[0, "raw text"], config.grc_byz1904_ΚΑΤΑ_ΜΑΡΚΟΝ_13_21)
+        self.assertEqual(
+            data.loc[1, "raw text"], config.grc_byz1904_ΚΑΤΑ_ΜΑΤΘΑΙΟΝ_24_23
+        )
+
         left_highlighted = " καὶ[yellow] τότε ἐάν τις ὑμῖν εἴπῃ, ἰδοὺ ὧδε ὁ Χριστός[/yellow], ἰδοὺ ἐκεῖ[yellow], μὴ πιστεύετε[/yellow]."
-        self.assertEqual(data.iat[0, 0], left_highlighted)
-        self.assertEqual(data.iloc[0]["Mark 13:21"], left_highlighted)
+        self.assertEqual(data.loc[0, "highlighted text"], left_highlighted)
         right_highlighted = "[yellow] τότε ἐάν τις ὑμῖν εἴπῃ, ἰδοὺ ὧδε ὁ Χριστὸς[/yellow] ἢ ὧδε[yellow], μὴ πιστεύσητε[/yellow]·"
-        self.assertEqual(data.iat[0, 1], right_highlighted)
-        self.assertEqual(data.iloc[0]["Matt. 24:23"], right_highlighted)
-        #TODO: summary = synopsis.getSummary()
-        #TODO: self.assertEqual(summary.iloc["count"]["Mark 13:21"], 14)
+        self.assertEqual(data.loc[1, "highlighted text"], right_highlighted)
 
 
 if __name__ == "__main__":
